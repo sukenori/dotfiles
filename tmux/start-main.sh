f080914ac719bfd6bc1fb8d9362a81da54806b89
ITO Sukenori <114732830+sukenori@users.noreply.github.com>
2026-03-09 20:46:01 +0900

    修正

--- tmux/start-main.sh
+++ tmux/start-main.sh
@@ -8,7 +8,7 @@ if tmux has-session -t "$SESSION" 2>/dev/null; then
 fi
 
 tmux new-session -d -s "$SESSION" -n editor
-tmux send-keys -t "$SESSION":editor.1 "nvim" C-m
+tmux send-keys -t "$SESSION":editor.1 "cd ~/atcoder-nim-env && nvim" C-m
 tmux split-window -v -l 8 -t "$SESSION":editor
 tmux select-pane -t "$SESSION":editor.1
 exec tmux attach -t "$SESSION"
