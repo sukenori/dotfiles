c5aca3df1ac535909b6ffcedf24fc3af13aec136
ITO Sukenori <114732830+sukenori@users.noreply.github.com>
2026-04-18 18:07:38 +0900

    Remove apt-get update command from setup script
    Removed unnecessary update command before installing Docker packages.

--- linux/setup.sh
+++ linux/setup.sh
@@ -13,7 +13,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # dotfiles ディレクトリ内にある、基盤となる Dockerfile をビルド
